930220ec08f50d53fd9345c51d0cc4c31b2117d1
Gordon Freeman <38136789+0x5066@users.noreply.github.com>
2020-11-17 16:06:31 +0100

    Add files via upload

---
+++ opensearch_desc.php
@@ -0,0 +1,8 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+<ShortName>ScWiki (English)</ShortName>
+<Description>ScWiki (English)</Description>
+<Image height="16" width="16" type="image/x-icon">http://wiki.skinconsortium.com/favicon.ico</Image>
+<Url type="text/html" method="get" template="http://wiki.skinconsortium.com/index.php?title=Special:Search&amp;search={searchTerms}"/>
+<Url type="application/x-suggestions+json" method="GET" template="http://wiki.skinconsortium.com/api.php?action=opensearch&amp;search={searchTerms}"/>
+</OpenSearchDescription>
